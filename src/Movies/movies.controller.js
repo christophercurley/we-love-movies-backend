6bdeff61ed6ff9b2571840461bde9f28d59b070b
Christopher Curley <clevercurley@gmail.com>
2022-10-04 08:16:54 -0400

    adds /movies and handles /movies?is_showing=true

--- src/Movies/movies.controller.js
+++ src/Movies/movies.controller.js
@@ -2,7 +2,9 @@ const moviesService = require("./movies.service");
 const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
 
 async function list(req, res) {
-  const data = await moviesService.list();
+  const is_showing = req.query.is_showing;
+  console.log("is_showing: ", is_showing);
+  const data = await moviesService.list(is_showing);
   res.json({ data });
 }
 
--- src/Movies/movies.service.js
+++ src/Movies/movies.service.js
@@ -1,8 +1,17 @@
 const knex = require("../db/connection");
 const mapProperties = require("../utils/map-properties");
 
-function list() {
-  return knex("movies").select("*");
+// I changed all movies with movie_id to "is_showing=false" in the movie_theaters table. If that causes problems with the tests, change back.
+
+function list(is_showing) {
+  return is_showing
+    ? knex("movies as m")
+        .join("movies_theaters as mt", "m.movie_id", "mt.movie_id")
+        .select("m.*")
+        .where({ is_showing: true })
+        .orderBy("movie_id")
+        .distinct()
+    : knex("movies").select("*");
 }
 
 module.exports = {
