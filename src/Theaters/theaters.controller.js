289ca856d2d3b7b3145e6665e1708965e47a0e38
Christopher Curley <clevercurley@gmail.com>
2022-10-04 15:08:22 -0400

    adds get /theaters route and passes tests

--- src/Theaters/theaters.controller.js
+++ src/Theaters/theaters.controller.js
@@ -1,7 +1,12 @@
-const theatersService = require("./movies.service");
+const theatersService = require("./theaters.service");
 const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
+const reduceProperties = require("../utils/reduce-properties");
 
-async function list() {}
+async function list(req, res) {
+  const data = await theatersService.list();
+  // console.log(data);
+  res.json({ data });
+}
 
 module.exports = {
   list: asyncErrorBoundary(list),
--- src/app.js
+++ src/app.js
@@ -2,10 +2,12 @@ if (process.env.USER) require("dotenv").config();
 const express = require("express");
 const app = express();
 const moviesRouter = require("./Movies/movies.router");
+const theatersRouter = require("./Theaters/theaters.router");
 
 app.use(express.json());
 
 app.use("/movies", moviesRouter);
+app.use("/theaters", theatersRouter);
 
 app.use((req, res, next) => {
   next({ status: 404, message: `Not found: ${req.originalUrl}` });
